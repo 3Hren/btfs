e797842e5a68c2d3c04dfeac7b696d0916c57a37
Evgeny Safronov <division494@gmail.com>
2018-08-06 19:02:31 +0200

    fix: add missing still required methods for Docker

--- src/btfs.cc
+++ src/btfs.cc
@@ -959,6 +959,21 @@ print_help() {
 }
 
 int
+btfs_chmod(const char *, mode_t) {
+    return 0;
+}
+
+int
+btfs_chown(const char *, uid_t, gid_t) {
+    return 0;
+}
+
+int
+btfs_utime(const char *, struct utimbuf *) {
+    return 0;
+}
+
+int
 main(int argc, char *argv[]) {
 	struct fuse_operations btfs_ops;
 	memset(&btfs_ops, 0, sizeof (btfs_ops));
@@ -972,6 +987,9 @@ main(int argc, char *argv[]) {
 	btfs_ops.getxattr = btfs_getxattr;
 	btfs_ops.init = btfs_init;
 	btfs_ops.destroy = btfs_destroy;
+        btfs_ops.chmod = btfs_chmod;
+        btfs_ops.chown = btfs_chown;
+        btfs_ops.utime = btfs_utime;
 
 	struct fuse_args args = FUSE_ARGS_INIT(argc, argv);
 
